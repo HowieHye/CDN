dde91017c57328a3ffa7ba037bd49dc411352076
Howiehye <howiehye@qq.com>
2020-07-30 21:58:44 +0800

    change git_push.sh

--- git_push.sh
+++ git_push.sh
@@ -1,11 +1,15 @@
+echo "Start submitting code to the local repository"
+git add .
+echo;
 unset msg
-
+echo "Commit the changes to the local repository"
 read -p "请输入commit提交描述：" msg
-
-git add -A
-
-git commit -m $msg
-
+git commit -m "$msg"
+echo;
+ 
+echo "Commit the changes to the remote git server"
 git push
-
-git status
+echo;
+ 
+echo "Batch execution complete!"
+echo;
